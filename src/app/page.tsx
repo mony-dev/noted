e6b9954f2a2b5479fb7a52166c8c6d30a9c9c85f
khamonluck <khamonluck.ang@gmail.com>
2025-05-26 06:35:40 +0700

    fix homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Please select a category to start</h1>
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-screen p-8 gap-8 bg-white text-mediumGray">
+      <h1 className="text-2xl font-bold">TODO List</h1>
+      <h2 className="text-2xl font-bold">Please select a category to start</h2>
+    </main>
   );
 }
